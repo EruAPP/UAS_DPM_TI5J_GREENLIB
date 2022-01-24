3b526e362a3a27cda5120ede76e12418eeb53ef9
Adrian Perdana Putra <adrian.spembi@gmail.com>
2022-01-24 17:25:20 +0700

    1911500001 Adrian Perdana Putra + Intent

--- app/src/main/java/ac/id/atmaluhur/mhs/GREENLIB/Akun.java
+++ app/src/main/java/ac/id/atmaluhur/mhs/GREENLIB/Akun.java
@@ -2,7 +2,9 @@ package ac.id.atmaluhur.mhs.GREENLIB;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Akun extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class Akun extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.akun);
+    }
+    public void login (View a){
+        startActivity(new Intent(Akun.this, Login.class));
     }
 }
